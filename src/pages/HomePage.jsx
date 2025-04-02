0cf735f0ebf81b41911ef818ff702eb2fccadafb
thanhite7 <thanhnguyentienusd@gmail.com>
2025-04-02 15:02:09 +0700

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import SpaProperties from '../components/SpaProperties'
-import SpaGallerySection from '../components/SpaGallerySection'
-import SpaFeatureServices from '../components/SpaFeatureServices'
-import LogoBar from '../components/LogoBar'
-import SpaAdvantage from '../components/SpaAdvantage'
-import SpaPricing from '../components/SpaPricing'
-import SpaVideoSection from '../components/SpaVideoSection'
-import SpaExpertTeam from '../components/SpaExpertTeam'
-import SpaAchievement from '../components/SpaAchievement'
-import SpaForm from '../components/SpaForm'
-import Footer from '../components/Footer'
-import SpaBlog from '../components/SpaBlog'
+import React from "react";
+import Hero from "../components/Hero";
+import SpaProperties from "../components/SpaProperties";
+import SpaGallerySection from "../components/SpaGallerySection";
+import SpaFeatureServices from "../components/SpaFeatureServices";
+import LogoBar from "../components/LogoBar";
+import SpaAdvantage from "../components/SpaAdvantage";
+import SpaPricing from "../components/SpaPricing";
+import SpaVideoSection from "../components/SpaVideoSection";
+import SpaExpertTeam from "../components/SpaExpertTeam";
+import SpaAchievement from "../components/SpaAchievement";
+import SpaForm from "../components/SpaForm";
+import Footer from "../components/Footer";
+import SpaBlog from "../components/SpaBlog";
+import BackGround from "/img/BackGround.svg";
+
 const HomePage = () => {
   return (
-    <div className='h-screen w-full p-0 m-0 text-[16px]'>
-        <Hero/>
-        <SpaProperties/>
-        <SpaGallerySection/>
-        <SpaFeatureServices/>
-        <LogoBar/>
-        <SpaAdvantage/>
-        <SpaPricing/>
-        <SpaVideoSection/>
-        <SpaExpertTeam/>
-        <SpaAchievement/>
-        <SpaForm/>
-        <SpaBlog/>
-        <Footer/>
+    <div 
+      className="min-h-screen w-full p-0 m-0 text-[16px] bg-no-repeat bg-fixed" 
+      style={{ 
+        backgroundImage: `url(${BackGround})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    >
+      <Hero />
+      <SpaProperties />
+      <SpaGallerySection />
+      <SpaFeatureServices />
+      <LogoBar />
+      <SpaAdvantage />
+      <SpaPricing />
+      <SpaVideoSection />
+      <SpaExpertTeam />
+      <SpaAchievement />
+      <SpaForm />
+      <SpaBlog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
