3d9a058f117d495ec1c49795a39f605d693e0739
thanhite7 <thanhnguyentienusd@gmail.com>
2025-04-02 09:20:02 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+
 // Get the directory name equivalent to __dirname in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -14,5 +15,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       'img': path.resolve(__dirname, './src/public/img')
     }
+  },
+  server: {
+    port:  5173, 
+    host: "0.0.0.0" 
   }
 })
