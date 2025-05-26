61ae0693fc7f97a782586464a6b43428bfdbc313
thanhite7 <thanhnguyentienusd@gmail.com>
2025-05-26 09:11:56 +0700

    fix: convert content to vietnamese

--- src/data/sliderImages.js
+++ src/data/sliderImages.js
@@ -1,10 +1,10 @@
 export const images = {
     "aboutUs": [
-        "/img/aboutUs/slider/_DSC6033.png",
-        "/img/aboutUs/slider/_DSC6041.png",
-        "/img/aboutUs/slider/DSC03569.png",
-        "/img/aboutUs/slider/DSC05147.png",
-        "/img/aboutUs/slider/DSC05151.png",
+        "/img/aboutUs/slider/_DSC6033.jpg",
+        "/img/aboutUs/slider/_DSC6041.jpg",
+        "/img/aboutUs/slider/DSC03569.jpg",
+        "/img/aboutUs/slider/DSC05151.jpg",
+        "/img/aboutUs/slider/DSC05151.jpg",
         "/img/aboutUs/slider/image2.svg",
         "/img/aboutUs/slider/image3.svg",
         "/img/aboutUs/slider/image4.svg",
