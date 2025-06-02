538d48348a963fd9bd9cac296cb7fd7b9d7c3145
thanhite7 <thanhnguyentienusd@gmail.com>
2025-06-02 13:23:23 +0700

    test

--- src/data/sliderImages.js
+++ src/data/sliderImages.js
@@ -1,11 +1,11 @@
 export const images = {
     "aboutUs": [
-        "/img/aboutUs/slider/_DSC6033.jpg",
-        "/img/aboutUs/slider/_DSC6041.jpg",
-        "/img/aboutUs/slider/DSC03569.jpg",
-        "/img/aboutUs/slider/DSC05147.jpg",
-        "/img/aboutUs/slider/DSC05151.jpg",
-        "/img/aboutUs/slider/DSC097412.png",
+        "/img/aboutUs/slider/_DSC6033.png",
+        "/img/aboutUs/slider/_DSC6041.png",
+        "/img/aboutUs/slider/DSC03569.png",
+        "/img/aboutUs/slider/DSC05147.png",
+        "/img/aboutUs/slider/DSC05151.png",
+        "/img/aboutUs/slider/DSC09741 (1).png",
 
     ], 
     "customerStories": [
